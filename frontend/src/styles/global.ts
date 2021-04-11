a476e097b43f4fb89fe2b32f51a03a82aa57567d
leoferreiralima <leoferreiralima@yahoo.com>
2021-04-11 08:54:25 -0300

    feat(frontend): inital commit

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #f1f1f0;
+    color: #333;
+    font-size: 16pt;
+  }
+
+`
