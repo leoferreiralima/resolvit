2a8bbf54f0c341682ea0574426a7d80df73b3430
Leoanardo Ferreira Lima <l.ferreira.lima@accenture.com>
2021-04-27 09:18:57 -0300

    adusjt background image

--- frontend/src/modules/layouts/LandingPage/styles.ts
+++ frontend/src/modules/layouts/LandingPage/styles.ts
@@ -4,6 +4,6 @@ export const Container = styled.div`
   background-image: url('/assets/landing-page-background.svg');
   background-repeat: no-repeat;
   height: 100%;
-  background-position: right;
+  background-position: left;
   background-size: cover;
 `
