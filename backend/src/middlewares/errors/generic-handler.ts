8b45747620b579d6af1d6816d0c28f73cfa6f1e9
leoferreiralima <leoferreiralima@yahoo.com>
2021-05-03 08:33:24 -0300

    add error log

--- backend/src/middlewares/errors/generic-handler.ts
+++ backend/src/middlewares/errors/generic-handler.ts
@@ -3,6 +3,7 @@ import { ErrorRequestHandler } from 'express'
 export const errorHandler: ErrorRequestHandler = (
   error: Error,_,res,__
 ): void => {
+  console.log(error)
   res.status(500).json({
     name: error.name,
     message: error.message
